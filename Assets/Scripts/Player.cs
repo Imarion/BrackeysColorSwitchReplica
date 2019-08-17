accabf57284ba9e95cb80297923e0dec467f966e
Manu <black_falcon00@hotmail.com>
2019-08-17 15:57:07 +0200

    Done

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float JumpForce = 10f;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+    public string currentColor;
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorPink;
+    public Color colorMagenta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRandomColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * JumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(collision.gameObject);
+            return;
+        }
+
+        if (collision.tag != currentColor)
+        {
+            Debug.Log("Game over");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void SetRandomColor()
+    {
+        int index = Random.Range(0, 4);
+
+        switch(index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                sr.color = colorCyan;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = colorYellow;
+                break;
+            case 2:
+                currentColor = "Pink";
+                sr.color = colorPink;
+                break;
+            case 3:
+                currentColor = "Magenta";
+                sr.color = colorMagenta;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
